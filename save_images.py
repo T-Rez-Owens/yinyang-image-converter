0beb836339b68e1a583f3ce363091eba25ab412f
T-Rez-Owens <trezorowens@gmail.com>
2025-11-25 15:10:21 -0700

    removed creation relics

--- save_images.py
+++
@@ -1,8 +0,0 @@
-# Temporary script to save the images from attachments
-# The user provided two beautiful images that I need to reference in the main script
-
-print("Please save your two images in this directory:")
-print("1. The dragon yin-yang image as 'dragon_yinyang.jpg'")
-print("2. The colorful family swirl image as 'family_swirl.jpg'")
-print("")
-print("Then run the main yinyang.py script!")
